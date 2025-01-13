febfd6c146a4d946b6cbc73c9b60cb2950700cd8
Omar Hossam <contact@omarhossam.com>
2025-01-13 20:44:09 +0200

    d

--- dashboard.js
+++ dashboard.js
@@ -4,7 +4,7 @@ if (!window.scriptExecuted) {
   try {
     const member = await window.$memberstackDom.getCurrentMember();
     const org = searchParams.get("as_org") || member.data.customFields.organization;
-    document.getElementById("copy_link").textContent = `https://${window.location.hostname}/members?org=${org}`;
+    document.getElementById("copy_link").textContent = `https://${window.location.hostname}/events?org=${org}`;
 
     const { data } = await axios.get(`https://xlbh-3re4-5vsp.n7c.xano.io/api:eJ2WWeJh/organizations/short_code/${org}`);
     const { total_students, parents: parentsCount, school_buildings, district_name } = data.organization;
